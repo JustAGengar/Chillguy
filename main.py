c7190a1a1dfce494cc1dc2db6ee8dca1352e6824
JustAGengar <henrytrannn@hotmail.com>
2025-01-21 10:07:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from web3 import Web3
 # Update the following variables with your own Etherscan and BscScan API keys and Telegram bot token
 ETHERSCAN_API_KEY = '<your_etherscan_api_key>'
 BSCSCAN_API_KEY = '<your_bscscan_api_key>'
-TELEGRAM_BOT_TOKEN = '<your_telegram_bot_token>'
+TELEGRAM_BOT_TOKEN = '<7993316774:AAHIpzteiW5qKtCD1qXZWw0riSZFMIdf4Ac>'
 TELEGRAM_CHAT_ID = '<your_telegram_chat_id>'
 
 # Define some helper functions
